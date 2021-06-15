c2df8fea76c41e79cc33485ab2aa0386077f6764
Anne Golladay <annegolladay@gmail.com>
2021-06-15 10:29:21 -0600

    added fixes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let rollbar = new Rollbar({
 })
 
 const app = express()
+app.use(express.json())
 let students = []
 
 app.get('/', (req, res) => {
@@ -18,9 +19,22 @@ app.get('/', (req, res) => {
 app.post('api/student', (req, res) => {
     let {name} = req.body
     name = name.trim()
-    students.push(name)
-    rollbar.log('student added successfully', {author: 'annie', type: 'manual'})
-    res.status(200).send(students)
+
+    const index = students.findIndex((studentName) => {
+        studentName === name
+    })
+
+    if (index === -1 && name !== '') {
+        students.push(name)
+        rollbar.log('student added successfully', {author: 'annie', type: 'manual'})
+        res.status(200).send(students)
+    } else if (name === '') {
+        rollbar.error('no name given')
+        res.status(400).send('must provide name')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 })
 
 const port = process.env.PORT || 4444
